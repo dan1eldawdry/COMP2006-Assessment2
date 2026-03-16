ca8a03b3b756c6ad50d28783ab629e937bbfde9d
Daniel Dawdry <daniel.dawdry@students.plymouth.ac.uk>
2026-03-16 17:20:09 +0000

    Improved database.py

--- database.py
+++ database.py
@@ -1,22 +1,25 @@
 import hashlib
 import secrets
 
-SHARED_SECRET = "secret_key" # the shared secret stored in both server and database
+class PasswordVaultDatabase:
+    def __init__(self, shared_secret):
+        self.shared_secret = shared_secret
 
-def generate_challenge():
-    challenge = secrets.token_hex(16)
-    print("database: generated challenge", challenge)
-    return challenge
+    def generate_challenge(self):
+        challenge = secrets.token_hex(16)
+        print("\n[Database] generated chllenge: ", challenge)
+        return challenge
+    
+    def verify_server(self, challenge, server_hash):
+        data = challenge + self.shared_secret
+        expected_hash = hashlib.sha256(data.encode()).hexdigest()
 
-def verify_response(challenge, server_hash):
-    data = challenge + SHARED_SECRET
-    expected_hash = hashlib.sha256(data.encode()).hexdigest()
-    print("database: expected hash: ", expected_hash)
-    print("database: recieved hash: ", server_hash)
+        print("[Database] expected hash: ", expected_hash)
+        print("[Database] recieved hash: ", server_hash)
 
-    if expected_hash == server_hash:
-        print("database: authentication SUCCESS")
-        return True
-    else:
-        print("database: authentication FAILED")
-        return False
+        if expected_hash == server_hash:
+            print("[Database] authentication success. server allowed access\n")
+            return True
+        else:
+            print("[Database] authentication failed. access denied\n")
+            return False
