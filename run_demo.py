b95a70c116121bfc95a57c21d0061ad54268c92f
Daniel Dawdry <daniel.dawdry@students.plymouth.ac.uk>
2026-03-16 17:32:27 +0000

    Improved run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -1,10 +1,20 @@
-import database
-import server
+from database import PasswordVaultDatabase
+from server import BackendServer
 
-print("server: requesting access to password vault")
+SHARED_SECRET = "secret_shared_key"
 
-challenge = database.generate_challenge # database sends challenge
+database = PasswordVaultDatabase(SHARED_SECRET)
+server = BackendServer(SHARED_SECRET)
 
-response = server.work_out_response(challenge) # server works out response
+print("==SERVER AUTHENTICATION DEMO==")
 
-database.verify_response(challenge, response) # database verifies
+print("\n[Server] requesting authentication with password vault") # server requests database access
+challenge = database.generate_challenge() # database sends challenge
+response = server.respond_to_challenge(challenge) # server works out response
+database.verify_server(challenge, response)
+
+print("==FAILED AUTH EXAMPLE==")
+fake_server = BackendServer("wrong_secret")
+challenge = database.generate_challenge()
+fake_response = fake_server.respond_to_challenge(challenge)
+database.verify_server(challenge, fake_response)
