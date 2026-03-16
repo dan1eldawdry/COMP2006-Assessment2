e175ee247bcdfb91b80b6f60e375ac7d42ebf22f
Daniel Dawdry <daniel.dawdry@students.plymouth.ac.uk>
2026-03-16 17:23:25 +0000

    Improved server.py

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import hashlib
 
-SHARED_SECRET = "secret_key" # same shared secret stored on server
+class BackendServer:
+    def __init__(self, shared_secret):
+        self.shared_secret = shared_secret
 
-def work_out_response(challenge):
-    data = challenge + SHARED_SECRET
-    response_hash = hashlib.sha256(data.encode()).hexdigest()
-    print("server: worked out response hash: ", response_hash)
-    return response_hash
+    def respond_to_challenge(self, challenge):
+        data = challenge + self.shared_secret
+        response_hash = hashlib.sha256(data.encode()).hexdigest()
+
+        print("[Server] worked out response hash: ", response_hash)
+
+        return response_hash
